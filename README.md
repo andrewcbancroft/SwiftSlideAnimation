02f1255173914f4c79189c532173eb8e3f81e49a
Antonio Medrano <antoniomedrano@users.noreply.github.com>
2017-03-19 11:39:41 -0700

    Update to Swift 3.0.1 (#2)
    * updates to work with Swift 3.0.1
    
    * Update project.pbxproj
    
    * Delete SwiftSlideAnimation.xcscheme
    
    * Delete xcschememanagement.plist
    
    * Update README.md

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 This repository contains an example XCode project for the blog post at [andrewcbancroft.com](http://www.andrewcbancroft.com) titled [Slide In Animation in Swift](http://www.andrewcbancroft.com/2014/09/24/slide-in-animation-in-swift/).
 
 ## Compatibility
-Verified that this repository's code works in XCode 6.3 with Swift 1.2
+Verified that this repository's code works in XCode 8.2.1 with Swift 3.0.1
--- SwiftSlideAnimation/UIViewExtensions.swift
+++ SwiftSlideAnimation/UIViewExtensions.swift
@@ -3,14 +3,14 @@ import UIKit
 extension UIView {
 	// Name this function in a way that makes sense to you... 
 	// slideFromLeft, slideRight, slideLeftToRight, etc. are great alternative names
-	func slideInFromLeft(duration: NSTimeInterval = 1.0, completionDelegate: AnyObject? = nil) {
+	func slideInFromLeft(_ duration: TimeInterval = 1.0, completionDelegate: AnyObject? = nil) {
 		// Create a CATransition animation
 		let slideInFromLeftTransition = CATransition()
 		
 		// Set its callback delegate to the completionDelegate that was provided (if any) 
-		if let delegate: AnyObject = completionDelegate {
-			slideInFromLeftTransition.delegate = delegate
-		}
+//		if let delegate: AnyObject = completionDelegate {
+//			slideInFromLeftTransition.delegate = delegate
+//		}
 		
 		// Customize the animation's properties
 		slideInFromLeftTransition.type = kCATransitionPush
@@ -20,6 +20,6 @@ extension UIView {
 		slideInFromLeftTransition.fillMode = kCAFillModeRemoved
 		
 		// Add the animation to the View's layer
-		self.layer.addAnimation(slideInFromLeftTransition, forKey: "slideInFromLeftTransition")
+		self.layer.add(slideInFromLeftTransition, forKey: "slideInFromLeftTransition")
 	}
 }
